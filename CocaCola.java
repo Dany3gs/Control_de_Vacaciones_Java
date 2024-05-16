21c11777cd5426c5e34414be926298b1252e7ba9
Dany <115885224+Dany3gs@users.noreply.github.com>
2024-05-16 22:35:54 +0100

    Ejercicio control de vacaciones

---
+++ CocaCola.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class CocaCola {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al sistema vacacional de Coca Cola Company");
+
+        Scanner nomEmpleado = new Scanner(System.in);
+            String nombre = "";
+                System.out.println("Cual es su nombre? ");
+                    nombre = nomEmpleado.nextLine();
+
+        Scanner claveDep = new Scanner(System.in);
+            int clave = 0;
+                System.out.println("Cual es su clave de departamento? ");
+                    clave = claveDep.nextInt();
+
+        Scanner añosServicio = new Scanner(System.in);
+            int antiguedad = 0;
+                System.out.println("Cuantos años lleva en la empresa? ");
+                    antiguedad = añosServicio.nextInt();
+        
+
+        if (clave == 1 && antiguedad >= 1 && antiguedad < 2) {
+            System.out.println("Al empleado " + nombre + " le pertencen 6 días");
+
+        }  else if (clave == 1 && antiguedad >= 2 && antiguedad <= 6) {
+            System.out.println("Al empleado " + nombre + " le pertenecen 14 días");
+
+        }  else if (clave == 1 && antiguedad >= 7) {
+            System.out.println("Al empleado  " + nombre + " le pertenecen 20 días");
+
+        }  else if (clave == 2 && antiguedad >= 1 && antiguedad < 2) {
+            System.out.println("Al empleado " + nombre + " le pertencen 7 días");
+
+        }  else if (clave == 2 && antiguedad >= 2 && antiguedad <= 6) {
+            System.out.println("Al empleado " + nombre + " le pertenecen 15 días");
+
+        }  else if (clave == 2 && antiguedad >= 7) {
+            System.out.println("Al empleado  " + nombre + " le pertenecen 22 días");
+
+        }  else if (clave == 3 && antiguedad >= 1 && antiguedad < 2) {
+            System.out.println("Al empleado " + nombre + " le pertencen 10 días");
+
+        }  else if (clave == 3 && antiguedad >= 2 && antiguedad <= 6) {
+            System.out.println("Al empleado " + nombre + " le pertenecen 20 días");
+
+        }  else if (clave == 3 && antiguedad >= 7) {
+            System.out.println("Al empleado  " + nombre + " le pertenecen 30 días");
+
+        }  else {
+            System.out.println("Aun no tiene derecho a vacaciones");
+        }
+
+    }
+}
